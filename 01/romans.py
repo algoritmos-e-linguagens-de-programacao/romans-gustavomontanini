3b69fed6747871c9a6c18291e14429e4e26118da
gustavomontanini <168143050+gustavomontanini@users.noreply.github.com>
2024-05-28 22:08:07 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,8 +1,38 @@
 def int_to_roman(num):
-    # Implemente sua função aqui
-    pass
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
 
+    roman_num = ''
+    i = 0
+    while  num > 0:
+        for _ in range(num // val[i]):
+            roman_num += roman[i]
+            num -= val[i]
+        i += 1
+    return roman_num
 
 def roman_to_int(s):
-    # Implemente sua função aqui
-    pass
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    result = 0
+    prev_value = 0
+    for char in s:
+        value = roman_dict[char]
+        if value > prev_value:
+            result += value - 2 * prev_value
+        else:
+            result += value
+        prev_value = value
+    return result
+
+
+#EXEMPLOS
+print(roman_to_int('XXV'))
+print(int_to_roman(689))
